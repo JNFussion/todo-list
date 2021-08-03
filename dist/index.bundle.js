7eb94c0077ef330c34a3adc092fd94eb82a59f96
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-08-03 18:01:16 +0200

    init

---
+++ dist/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('ehhh')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsbUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2VoaGgnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
