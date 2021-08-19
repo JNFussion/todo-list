25fb998cd030a022faf2e78e1584470cdb7d257d
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-08-19 12:11:41 +0200

    Split main.scss into partials. Disable caching from config. Add mustache and date-fns

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  cache: false,
   mode: 'development',
   entry: {
     index:'./src/index.js',
