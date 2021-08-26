ba80a483c1cd5b7c3dd26d40da30dad07ae915a7
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-08-26 17:11:04 +0200

    add buttons tag, add enum.

--- src/helper.js
+++ src/helper.js
@@ -6,4 +6,10 @@ const toggleHide = (elementToHide) => {
   elementToHide.classList.toggle('hide');
 }
 
-export {hide, toggleHide}
+const priorityEnum ={
+  HIGHT: {weight: 2, name: "hight" },
+  MEDIUM: {weight: 1, name: "medium"},
+  LOW: {weight: 0, name: "low"}
+}
+
+export {hide, toggleHide, priorityEnum}
