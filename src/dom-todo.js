e32d8c71de95ff798dfa9d1178e13a2ce406c190
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-08-26 12:46:59 +0200

    Fix bug rendering first todo, todos can now be delete.

--- src/dom-todo.js
+++ src/dom-todo.js
@@ -6,10 +6,10 @@ const Mustache = require('mustache');
 
 const domTodo = (() => {
   const target = document.querySelector('.todo-list');
-  const newTodoBtn = target.querySelector('#new-todo');
+  const newTodoBtn = document.querySelector('#new-todo');
   const todoTemplate = document.getElementById('todo-template').innerHTML;
   const formTemplate = document.getElementById('todo-form-template').innerHTML;
-
+  let noTodoFlag = true;
 
 
   const _article = () => {
@@ -18,15 +18,19 @@ const domTodo = (() => {
     return article;
   }
 
+  const getIndexTodo = (btn) => {
+    let list = Array.from(target.querySelectorAll('.todo'));
+    let max = list.length - 1;
+    let index = max - list.indexOf(btn.closest('.todo'));
+    pubsub.publish('deleteTodo', index);
+  }
+
   const removeForm = () => {
     target.removeChild(target.querySelector('#form-todo').parentElement);
   }
 
   const removeTodos = () => {
-    let newTodoBtn = target.querySelector('#new-todo');
     target.textContent = '';
-    console.log(newTodoBtn)
-    target.append(newTodoBtn);
   }
 
   const contractAll = () => {
@@ -38,10 +42,18 @@ const domTodo = (() => {
   }
 
   const renderIndex = (project) => {
-    project.getTodoList().forEach(todo => domTodo.renderShow(todo));
+    removeTodos();
+    if(project.getTodoList().length == 0){
+      renderShow(null);
+      noTodoFlag = true;
+    }else {
+      project.getTodoList().forEach(todo => renderShow(todo));
+    }
   }
 
   const renderShow = (todo) => {
+    noTodoFlag && removeTodos();
+    noTodoFlag = false;
     let newArticle = _article();
     newArticle.innerHTML = Mustache.render(todoTemplate, { todo: todo})
     target.prepend(newArticle);
@@ -64,9 +76,11 @@ const domTodo = (() => {
   pubsub.subscribe('newTodo', contractAll);
   pubsub.subscribe('newTodo', toggleNewBtn);
   pubsub.subscribe('newTodo', renderNew);
+  pubsub.subscribe('deleteButtonTodo', getIndexTodo);
   pubsub.subscribe('createTodo', removeForm);
   pubsub.subscribe('createTodo', toggleNewBtn)
   pubsub.subscribe('createTodo', renderShow);
+  pubsub.subscribe('sortedTodos', renderIndex);
 
   return {renderShow, renderNew, renderEdit, removeForm}
 })();
