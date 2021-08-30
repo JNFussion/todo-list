257f7e91bef0a177a3389c4e21b8c648d2d93816
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-08-30 13:32:12 +0200

    add ids attribute using nanoid, remove updated_at attribute, fix wrong output on getFormatedDueDate when duedate has any time. Add functions to get a string of the date or time  of dueDate. Add sort functions to projectFactory (by creation, priority and dueDate).

--- src/project.js
+++ src/project.js
@@ -1,9 +1,8 @@
-import { format, isFuture, isToday } from "date-fns";
+import { format, getHours, isFuture, isToday, compareAsc, compareDesc } from "date-fns";
 import isValid from "date-fns/isValid";
 
+const proto = (title, description, priority, dueDate) => {
 
-const proto = (title, description, priority, dueDate, created_at) => {
- 
   const _isValidDate = () => {
     return !isValid(dueDate) || isFuture(dueDate);
   }
@@ -15,26 +14,38 @@ const proto = (title, description, priority, dueDate, created_at) => {
       if(isToday(dueDate)){
         return format(dueDate, 'kk:mm');
       }
+      if(getHours(dueDate) == 0) {
+        return format(dueDate, "dd MMM Y");
+      }
       return format(dueDate, "dd MMM Y 'at' kk:mm");
     }
     return 'No deadline';
   }
+  const getDate = () => {
+    if(!dueDate) return "";
+    return format(dueDate, "Y-MM-dd");
+  }
+  const getTime = () => {
+    if(!dueDate) return "";
+    return format(dueDate, "kk:mm")
+  }
   return Object.assign(
-    { title: title, 
+    { 
+      id : null,
+      title: title, 
       description: description, 
       priority: priority, 
       dueDate: dueDate, 
-      created_at : created_at, 
-      updated_at : created_at },
-    { isObjectValid, getFormatedDueDate })
+      created_at : null,},
+    {isObjectValid, getFormatedDueDate, getDate, getTime })
 }
 
-const todoFactory = (title, description, priority, dueDate, created_at, project) => {
-  const todo = { checked: false, project: project }
-  return Object.assign(todo, proto(title, description, priority, dueDate ,created_at));
+const todoFactory = (title, description, priority, dueDate) => {
+  const todo = { checked: false}
+  return Object.assign(todo, proto(title, description, priority, dueDate));
 }
 
-const projectFactory = (title, description, priority, dueDate, created_at) => {
+const projectFactory = (title, description, priority, dueDate) => {
   let todoList = [];
   
   const isCheck = () => {
@@ -42,12 +53,46 @@ const projectFactory = (title, description, priority, dueDate, created_at) => {
   }
 
   const getTodoList = () => todoList;
+  const setTodoList = (list) => todoList = list;
 
   const addTodoItem = (todoItem) => {
     todoList.push(todoItem);
   }
-  const project = { isCheck, getTodoList, addTodoItem }
-  return Object.assign(project, proto(title, description, priority, dueDate ,created_at));
+  
+  const removeTodoItem = (index) => {
+    todoList.splice(index, 1);
+  }
+
+  const sortByCreatedAt = (direction) => {
+    if(direction){
+      todoList.sort((a, b) => compareAsc(a.created_at, b.created_at));
+    } else {
+      todoList.sort((a, b) => compareDesc(a.created_at, b.created_at));
+    }
+  }
+
+  const sortByPriority = (direction) => {
+    if(direction) {
+      todoList.sort((a, b) => a.priority.weight > b.priority.weight);
+    }else {
+      todoList.sort((a, b) => a.priority.weight < b.priority.weight);
+    }
+  }
+
+  const sortByDueDate = (direction) => {
+    if(direction) {
+      todoList.sort((a, b) => compareDesc(a.dueDate, b.dueDate));
+    } else {
+      todoList.sort((a, b) => compareAsc(a.dueDate, b.dueDate));
+    }
+  }
+
+  const reverseList = () => {
+    todoList.reverse()
+  }
+
+  const project = { isCheck, getTodoList, setTodoList, addTodoItem, removeTodoItem, sortByCreatedAt, sortByPriority, sortByDueDate, reverseList }
+  return Object.assign(project, proto(title, description, priority, dueDate));
 }
 
 export {todoFactory, projectFactory};
