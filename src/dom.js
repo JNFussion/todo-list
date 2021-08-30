cbcf266b9ec10b862ae485d3f6238d4c51a85e4f
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-08-30 13:44:37 +0200

    Add function to check if there is a form, add events to the sort buttons, edit buttons and  delete buttons. Update _createItem to remove create_at and update_at, create_at is set after validation. Update flow control of submit buttons to differentiate of updates or creations.

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,11 @@
-import { parseISO, set } from 'date-fns';
+import { parseISO, set, startOfYesterday } from 'date-fns';
 import {domProject} from './dom-project';
 import {domTodo} from './dom-todo';
 import { projectFactory, todoFactory } from './project';
 import { pubsub } from './pubsub';
-import { hide, toggleHide } from './helper';
+import { hide, priorityEnum, toggleHide } from './helper';
+import { DomBarSettings } from './dom-bar-settings';
+import { nanoid } from 'nanoid';
 
 const domManager = (() => {
 
@@ -19,20 +21,34 @@ const domManager = (() => {
     let form  = document.getElementById(`form-${type}`);
     let formData = new FormData(form);
     let dueDate = _getFullDate(formData.get(`${type}[dueDate]-date`),formData.get(`${type}[dueDate]-time`));
-    
     if(type == 'project'){
-      return projectFactory( formData.get(`${type}[title]`), 
+      return projectFactory(
+        formData.get(`${type}[title]`), 
         formData.get(`${type}[description]`), 
-        formData.get(`${type}[priority]`), 
-        dueDate, 
-        new Date());
+        priorityEnum[formData.get(`${type}[priority]`)],
+        dueDate);
     }
-
-    return todoFactory(formData.get(`${type}[title]`), 
+    return todoFactory(
+      formData.get(`${type}[title]`), 
       formData.get(`${type}[description]`), 
-      formData.get(`${type}[priority]`), 
-      dueDate, 
-      new Date());
+      priorityEnum[formData.get(`${type}[priority]`)], 
+      dueDate);
+  }
+
+  const clearSelection = () => {
+    if(document.selection && document.selection.empty) {
+        document.selection.empty();
+    } else if(window.getSelection) {
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+    }
+  }
+
+  const isThereForm = () => {
+    if(document.querySelectorAll('#form-todo, #form-project').length == 0) {
+      return false
+    }
+    return true;
   }
 
   /* EVENTS */
@@ -43,41 +59,114 @@ const domManager = (() => {
         pubsub.publish('newProject');
         break;
       case 'new-todo':
-        pubsub.publish('newTodo');
+        if(isThereForm()) break;
+
+        pubsub.publish('newTodo', projectFactory());
+        break;
+      case 'sortCreatedAt':
+        if(e.target.classList.contains('current')) break;
+        pubsub.publish('sortTodosOfProject', 'sortByCreatedAt');
+        pubsub.publish('swapSelectBarOptions', e.target);
+        break;
+      case 'sortPriority':
+        if(e.target.classList.contains('current')) break;
+        pubsub.publish('sortTodosOfProject', 'sortByPriority');
+        pubsub.publish('swapSelectBarOptions', e.target)
+        break;
+      case 'sortDueDate':
+        if(e.target.classList.contains('current')) break;
+        pubsub.publish('sortTodosOfProject', 'sortByDueDate');
+        pubsub.publish('swapSelectBarOptions', e.target)
+        break;
+      case 'asc':
+        if(e.target.classList.contains('current')) break;
+        pubsub.publish('swapSelectBarOptions', e.target);
+        pubsub.publish('swapOrderOption', false);
+        break;
+      case 'desc':
+        if(e.target.classList.contains('current')) break;
+        pubsub.publish('swapSelectBarOptions', e.target)
+        pubsub.publish('swapOrderOption', true);
         break;
     }
     if(e.target && e.target.className.split(' ').some(c => /fa-angle-*/.test(c))) {
       if(e.target.className.split(' ').some(c => /fa-angle-(up|down)/.test(c))) {
-        toggleHide(e.target.closest('.todo').querySelector('.description-wrapper'))
-        e.target.classList.replace('fa-angle-up', 'fa-angle-down') || e.target.classList.replace('fa-angle-down', 'fa-angle-up')
+        toggleHide(e.target.closest('.todo').querySelector('.description-wrapper'));
+        e.target.classList.replace('fa-angle-up', 'fa-angle-down') || e.target.classList.replace('fa-angle-down', 'fa-angle-up');
       }else if(e.target.className.split(' ').some(c => /fa-angle-(right|down)/.test(c))){
-        e.target.classList.replace('fa-angle-right', 'fa-angle-down') || e.target.classList.replace('fa-angle-down', 'fa-angle-right')
+        e.target.classList.replace('fa-angle-right', 'fa-angle-down') || e.target.classList.replace('fa-angle-down', 'fa-angle-right');
       }
     }
+    let item = e.target.closest('.todo');
+    if(e.target && e.target.classList.contains('fa-edit')){
+      if(isThereForm()) return false;
+      if(item){
+        pubsub.publish('editTodo', item.id)
+      }else {
+        pubsub.publish('editProject', e.target.closest('.project').id);
+      }
+    } else if(e.target && e.target.classList.contains('fa-trash')) {
+      if(isThereForm()) return false;
+      if (window.confirm("Are you sure you want to delete this task?")) {
+        if(item){
+          pubsub.publish('deleteTodo', item.id);
+        }else {
+          pubsub.publish('deleteProject', e.target.closest('.project').id);
+        }
+      }
+    }
+
   });
 
   document.addEventListener('submit', (e) => {
+    e.preventDefault()
     switch (e.target && e.target.id) {
       case 'form-project':
         let project = _createItem('project');
-        if (project.isObjectValid()){
-          pubsub.publish('createProject', project);
+        let projectId = e.target.closest('.project').id;
+        if(projectId){
+          project.id = projectId;
+          if(project.isObjectValid()){
+            pubsub.publish('updateProject', project);
+          }else {
+            pubsub.publish('renderEditProject', project)
+          }
         }else {
-          pubsub.publish('editProject', project)
+          if(project.isObjectValid()){
+            project.id = nanoid();
+            project.created_at = new Date()
+            pubsub.publish('createProject', project);
+          }else {
+            pubsub.publish('newProject', project);
+          }
         }
         break;
       case 'form-todo':
         let todo = _createItem('todo');
-        if (todo.isObjectValid()){
-          pubsub.publish('createTodo', todo);
-        }else {
-          pubsub.publish('editProject', todo)
+        let todoId = e.target.closest('.todo').id;
+        if(todoId){
+          todo.id = todoId;
+          if(todo.isObjectValid()){
+            pubsub.publish('updateTodo', todo);
+          }else {
+            pubsub.publish('renderEditTodo', todo);
+          }
+        } else {
+          if(todo.isObjectValid()){
+            todo.id = nanoid();
+            todo.created_at = new Date()
+            pubsub.publish('createTodo', todo);
+          }else {
+            pubsub.publish('newTodo', todo);
+          }
         }
         break;
     }
-    e.preventDefault();
   })
 
+  document.addEventListener('dblclick', e => {
+    clearSelection();
+  })
 })();
 
 export {domManager};
