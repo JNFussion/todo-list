28708288d03476994a72902cd8c2bdb4351ff4f9
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-09-07 18:25:45 +0200

    Add function to fetch all todos and sort them by the proper view type, add function to CRUD projects locally. Minor changes due to change in html.

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,44 +1,18 @@
 import { pubsub } from "./pubsub";
 import {projectFactory, todoFactory} from './project';
-import {priorityEnum} from './helper';
-import {endOfTomorrow, set, startOfTomorrow, startOfYesterday } from 'date-fns';
-import { domProject } from "./dom-project";
-import { nanoid } from "nanoid";
-
-let des = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel justo nulla. Aenean non nisi vel tellus dignissim scelerisque eget sed erat. Integer dignissim turpis semper dui dapibus condimentum. Donec ut tortor orci. Vivamus ornare lacus dui, nec facilisis leo luctus nec. Nullam egestas convallis iaculis.'
-
-let project = projectFactory('Project1', des, 2, endOfTomorrow());
-let project2 = projectFactory('Project2', des,  2, startOfTomorrow());
-
-let todos = [todoFactory('Todo', des, priorityEnum.MEDIUM, new Date(2021,8,24,14,0), project), todoFactory('Todo', des, priorityEnum.LOW, new Date(2021,7,28,18,0), project), todoFactory('Todo', des, priorityEnum.HIGHT, startOfTomorrow(), project)];
-let todos2 = [todoFactory('TASK', des, priorityEnum.MEDIUM, new Date(2021,8,24,14,0), project2), todoFactory('Another Task', des, priorityEnum.MEDIUM, new Date(2021,7,28,18,0), project2), todoFactory('Another another task', des, priorityEnum.HIGHT, startOfTomorrow(), project2)];
-
-todos.forEach(item => {
-  let date = new Date(2021,7,(Math.floor(Math.random() * (26 - 23 + 1) ) + 23),14,0)
-  item.created_at = date;
-  item.id = 'todo-' + nanoid()
-  project.addTodoItem(item)
-});
-
-todos2.forEach(item => {
-  let date = new Date(2021,7,(Math.floor(Math.random() * (26 - 23 + 1) ) + 23),14,0)
-  item.created_at = date;
-  item.id = 'todo-' + nanoid()
-  project2.addTodoItem(item);
-});
-
-project.id ='project-' + nanoid()
-project2.id ='project-' + nanoid()
+import {add, compareAsc, compareDesc, isAfter, isBefore, isToday, startOfTomorrow } from 'date-fns';
 
 const dashboard = (() => {
   let projectsList = [];
-  let currentProject = project;
+  let currentProject = undefined;
   let orderDesc = true;
 
+  const getProjects = () => projectsList;
   const addProject = (project) => projectsList.push(project)
   const addTodoToCurrentProject = (todo) => currentProject.addTodoItem(todo) 
   const getCurrentProject = () => currentProject;
   const setCurrentProject = (project) => currentProject = project;
+  const setProjectToTodo = (todo) => todo.project = currentProject; 
   const setOrder = (order) => orderDesc = order;
   
   const removeCurrentFromProjectList = () => {
@@ -59,7 +33,14 @@ const dashboard = (() => {
     }
   }
   const editTodo = (id) => {
-    pubsub.publish('renderEditTodo', currentProject.getTodoList().filter(todo => todo.id == id)[0]);
+    let wantedTodo = {};
+    for (const p of projectsList) {
+      for (const t of p.getTodoList()) {
+        if(t.id == id) wantedTodo = t;
+      }
+    }
+    pubsub.publish('renderEditTodo', wantedTodo);
+    return wantedTodo
   }
 
   const updateTodo = (todo) => {
@@ -91,39 +72,108 @@ const dashboard = (() => {
 
   const updateOrderList = () => {
     currentProject.reverseList();
-    pubsub.publish('showProject', currentProject);
+    pubsub.publish('sortedTodos', currentProject);
   }
 
   const sortCurrentProject = (byType) => {
     currentProject[byType](orderDesc);
-    pubsub.publish('showProject', currentProject);
+    pubsub.publish('sortedTodos', currentProject);
   }
 
   const goToProject = (id) => {
-    if(currentProject.id == id) return ;
+    if( currentProject && currentProject.id == id ) return ;
     let projectWanted = projectsList.find(p => p.id == id);
     setCurrentProject(projectWanted);
     pubsub.publish('showProject', currentProject);
   }
 
+  const sortBy = (byType, orderDesc) => {
+    let allTodos = [];
+    projectsList.forEach(p => allTodos = allTodos.concat(p.getTodoList()))
+
+    switch(byType) {
+      case 'today':
+        allTodos = allTodos.filter(t => isToday(t.dueDate))
+        allTodos.sort((a, b) => compareDesc(a.dueDate, b.dueDate))
+      case 'upcoming':
+        allTodos = allTodos.filter(t => !isAfter(t.dueDate, startOfTomorrow()) && !isBefore(t.dueDate, add(Date.now, {days: 7})));
+        allTodos.sort((a, b) => (orderDesc ? compareDesc(a.dueDate, b.dueDate) : compareAsc(a.dueDate, b.dueDate)) || b.priority.weight - a.priority.weight)
+        break;
+      case 'priority': 
+        allTodos.sort((a, b) =>  (orderDesc ?  b.priority.weight - a.priority.weight : a.priority.weight - b.priority.weight) || compareAsc(a.dueDate, b.dueDate))
+        break;
+    }
+    return allTodos;
+  }
+
+  const _projectToString = (project) => {
+    let projectToSaveWithTodos = {
+      ...project,
+      todos: project.getTodoList()
+    }
+    return JSON.stringify(projectToSaveWithTodos)
+  }
+
+  const loadLocalProjects = () => {
+    projectsList.length = 0;
+    
+    for (var i = 0; i < localStorage.length; i++){
+      let tempObj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      let newProject = projectFactory(tempObj.title, tempObj.description, tempObj.priority, new Date(tempObj.dueDate));
+      newProject.id = tempObj.id;
+      newProject.created_at = new Date(tempObj.created_at);
+      
+      tempObj.todos.forEach(t => {
+        let newTodo = todoFactory(t.title, t.description, t.priority, new Date(t.dueDate), newProject);
+        newTodo.id = t.id;
+        newTodo.created_at = new Date(t.created_at);
+        newProject.addTodoItem(newTodo);
+      })
+      addProject(newProject);
+    }
+  }
+
+  const saveLocalProject = (item) => {
+    let projectsToSave = undefined
+    if(item.project) {
+      projectsToSave = item.project;
+    }else if(item.id) {
+      projectsToSave = item;
+    }else {
+      projectsToSave = projectsList.find(p => p.getTodoList().find(t => t.id == item))
+      projectsToSave.getTodoList().splice(projectsToSave.getTodoList().findIndex(t => t.id == item), 1);
+    }
+    localStorage.removeItem(projectsToSave.id);
+    localStorage.setItem(projectsToSave.id, _projectToString(projectsToSave));
+  }
+
+  const removeLocalProject = (id) => {
+    localStorage.removeItem(id);
+  }
+
   pubsub.subscribe('createProject', addProject);
   pubsub.subscribe('createProject', setCurrentProject);
+  pubsub.subscribe('createProject', saveLocalProject);
   pubsub.subscribe('editProject', editProject);
   pubsub.subscribe('updateProject', updateProject);
+  pubsub.subscribe('updateProject', saveLocalProject)
   pubsub.subscribe('deleteProject', deleteProject);
+  pubsub.subscribe('deleteProject', removeLocalProject);
+  pubsub.subscribe('createTodo', setProjectToTodo);
   pubsub.subscribe('createTodo', addTodoToCurrentProject);
+  pubsub.subscribe('createTodo', saveLocalProject);
   pubsub.subscribe('editTodo', editTodo);
-  pubsub.subscribe('updateTodo', updateTodo)
+  pubsub.subscribe('updateTodo', updateTodo);
+  pubsub.subscribe('updateTodo', saveLocalProject)
+  pubsub.subscribe('deleteTodo', saveLocalProject);
   pubsub.subscribe('deleteTodo', removeTodoFromCurrentProject);
   pubsub.subscribe('sortTodosOfProject',sortCurrentProject);
   pubsub.subscribe('swapOrderOption', setOrder);
   pubsub.subscribe('swapOrderOption', updateOrderList);
-  pubsub.subscribe('navbarProject', goToProject);
+  pubsub.subscribe('projectClicked', goToProject);
+  pubsub.subscribe('noCurrentProject', setCurrentProject);
 
-  return { projectsList, addProject, getCurrentProject, setCurrentProject, sortCurrentProject };
+  return { getProjects, addProject, getCurrentProject, setCurrentProject, sortCurrentProject, sortBy, loadLocalProjects };
 })();
-
-pubsub.publish('createProject', project)
-pubsub.publish('createProject', project2)
 
 export { dashboard };
